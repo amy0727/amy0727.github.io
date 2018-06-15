e050e6892c934a2e7e89bf297f3d891de3f4b162
jimmylee850328 <jimmylee850328@gmail.com>
2018-06-15 17:28:14 +0800

    add facebook folder

---
+++ facebook/js/script.js
@@ -0,0 +1,32 @@
+// Fast Loading
+function init() {
+	var vidDefer = document.getElementsByTagName('iframe');
+	for (var i = 0; i < vidDefer.length; i++) {
+		if (vidDefer[i].getAttribute('data-src')) {
+			vidDefer[i].setAttribute('src', vidDefer[i].getAttribute('data-src'));
+		}
+	}
+}
+window.onload = init;
+// drag move
+$(function() {
+	$("#fb_msg").draggable({
+		containment: "window"
+	});
+});
+// BUTTON
+$(function() {
+	$("#addClass").click(function() {
+		$('#fb_msg').addClass('popup-on');
+	});
+    
+    $("#removeClass").click(function() {
+		$('#fb_msg').removeClass('popup-on');
+    });
+    
+	$('#fb_msg').addClass('popup-on');
+
+	// setTimeout(function() {
+	// 	$('#fb_msg').addClass('popup-on');
+	// }, 3000);
+})
